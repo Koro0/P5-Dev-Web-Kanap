a0ffb87ea02870f9f15267079e06173fde4df9a1
Koro0 <ledane92@gmail.com>
2022-06-27 14:20:49 +0200

    review code

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,79 +1,131 @@
-fetch('http://localhost:3000/api/products')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    // Work with JSON data here
-    showsOrder(data);
-  })
-  .catch((err) => {
-    // Do something for an error here
-    console.log(err);
-  });
+let cart = JSON.parse(localStorage['productsInCart']);
+console.log(cart);
+let sectionCart = document.getElementById('cart__items');
 
-function showsOrder(data) {
-  let articleOrder = document.getElementById('cart__items');
+for (i = 0; i < cart.length; i++) {
+  let itemId = cart[i].id;
+  //console.log(itemId);
+  let itemColor = cart[i].option;
+  //console.log(itemColor);
+  let itemQuantity = cart[i].quantity;
+  //console.log(itemQuantity);
 
-  //recupere les produits dans le localStorage
-  let orders = JSON.parse(localStorage['productsInCart']);
-  console.log(orders);
-  for (j = 0; j < orders.length; j++) {
-    for (i = 0; i < data.length; i++) {
-      if (orders[j].id == data[i]._id) {
-        let baliseArticle = document.createElement('article');
-        baliseArticle.className = 'cart__item';
-        baliseArticle.setAttribute('data-id', data[i]._id);
-        baliseArticle.setAttribute('data-color', data[i].color);
-        //console.log(baliseArticle);
-        /////////////////////////////////////////////////////
-        let artcileImg =
-          '<div class="cart__item__img"> <img src="' +
-          data[i].imageUrl +
-          '" alt="' +
-          data[i].altTxt +
-          '"> </div>';
-        let articleContent = '<div class="cart__item__content">';
-        let articleDescription =
-          '<div class="cart__item__content__description"> <h2>' +
-          data[i].name +
-          '</h2> <p> ' +
-          orders[j].option +
-          '</p> <p>' +
-          data[i].price +
-          ' € </p>  </div>';
-        let articleContentSettings =
-          ' <div class="cart__item__content__settings">';
-        let articleContentSettingsQuantity =
-          ' <div class="cart__item__content__settings__quantity"> <p>Qté :' +
-          '</p> <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="' +
-          orders[j].quantity +
-          '"> </div> <div class="cart__item__content__settings__delete"> <p class="deleteItem">Supprimer</p>  </div>';
-        articleContentSettings = articleContentSettingsQuantity + '</div>';
-        articleContent = articleDescription + articleContentSettings + '</div>';
-        baliseArticle.innerHTML = artcileImg + articleContent;
-        console.log(baliseArticle);
-        ///////////////////////////////////////////////////////
-        articleOrder.appendChild(baliseArticle);
-        ////////////////////////////////////////////////////
-        let articleQuantity = document.querySelector('.itemQuantity');
-        //console.log(articleQuantity.value);
-        let artcileDelete = document.querySelector('.deleteItem');
+  ////////////////reque fetch selon l'item/////////////
+  let requete = 'http://localhost:3000/api/products/' + itemId;
+  //console.log(requete);
+  fetch(requete)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // Work with JSON data here
+      ///////////////////////////// img ///////////////////////////////////////////////////
+      let cartItemImgUnder = document.createElement('img');
+      cartItemImgUnder.src = data.imageUrl;
+      cartItemImgUnder.alt = data.altTxt;
+      //console.log(cartItemImgUnder);
 
-        articleQuantity.addEventListener('mouseup', function (event) {
-          if (articleQuantity.value < 0) {
-            orders.splice(j, 1);
-            orders[j].push({
-              id: data[i]._id,
-              quantity: parseInt(event.target.value),
-              option: ord,
-            });
-            alert('§§');
-          }
-          console.log("orders de l'/Event" + orders[j]);
-        });
-        console.log('msg : ' + orders[j].quantity);
-        console.log("orders de l'/Event", orders[j]);
-      }
-    }
-  }
+      let cartItemImg = document.createElement('div');
+      cartItemImg.className = 'cart__item__img';
+      //console.log(cartItemImg);
+
+      cartItemImg.appendChild(cartItemImgUnder);
+
+      /////////////////////////// Item Content /////////////////////////////////////////
+
+      /////////////////////////// Item Content Decriptions ////////////////////////////
+
+      let cartItemName = document.createElement('h2');
+      cartItemName.textContent = data.name;
+      //console.log(cartItemName);
+
+      let cartItemColor = document.createElement('p');
+      cartItemColor.textContent = itemColor;
+      //console.log(cartItemColor);
+
+      let cartItemPrice = document.createElement('p');
+      cartItemPrice.textContent = data.price + ' €';
+      //console.log(cartItemPrice);
+
+      let cartItemContentDescription = document.createElement('div');
+      cartItemContentDescription.className = 'cart__item__content__description';
+      //console.log(cartItemContentDescription);
+
+      cartItemContentDescription.appendChild(cartItemName);
+      cartItemContentDescription.appendChild(cartItemColor);
+      cartItemContentDescription.appendChild(cartItemPrice);
+      ///////////////////////////// Item Content Settings /////////////////////////////
+
+      //////////////////////////// Item Content Settings_Quantity ////////////////////
+
+      let cartItemContentSettingsQuantityUnder = document.createElement('p');
+      cartItemContentSettingsQuantityUnder.textContent = 'Qté : ';
+      //console.log(cartItemContentSettingsQuantityUnder);
+
+      let cartItemContentSettingsQuantityInput =
+        document.createElement('input');
+      cartItemContentSettingsQuantityInput.type = 'number';
+      cartItemContentSettingsQuantityInput.className = 'itemQuantity';
+      cartItemContentSettingsQuantityInput.name = 'itemQuantity';
+      cartItemContentSettingsQuantityInput.min = 1;
+      cartItemContentSettingsQuantityInput.max = 100;
+      cartItemContentSettingsQuantityInput.value = itemQuantity;
+      //console.log(cartItemContentSettingsQuantityInput);
+
+      let cartItemContentSettingsQuantity = document.createElement('div');
+      cartItemContentSettingsQuantity.className =
+        'cart__item__content__settings__quantity';
+
+      cartItemContentSettingsQuantity.appendChild(
+        cartItemContentSettingsQuantityUnder
+      );
+      cartItemContentSettingsQuantity.appendChild(
+        cartItemContentSettingsQuantityInput
+      );
+
+      //////////////////////////// Item Content Settings_Delete ///////////////////////
+
+      let cartItemContentSettingsDeleteUnder = document.createElement('p');
+      cartItemContentSettingsDeleteUnder.className = 'deleteItem';
+      cartItemContentSettingsDeleteUnder.textContent = 'Supprimer';
+      //console.log(cartItemContentSettingsDeleteUnder);
+
+      let cartItemContentSettingsDelete = document.createElement('div');
+      cartItemContentSettingsDelete.className =
+        'cart__item__content__settings__delete';
+
+      cartItemContentSettingsDelete.appendChild(
+        cartItemContentSettingsDeleteUnder
+      );
+      ///////////////////////////////////////////////////////////////////////////////////
+
+      let cartItemContentSettings = document.createElement('div');
+      cartItemContentSettings.className = 'cart__item__content__settings';
+      //console.log(cartItemContentSettings);
+
+      cartItemContentSettings.appendChild(cartItemContentSettingsQuantity);
+      cartItemContentSettings.appendChild(cartItemContentSettingsDelete);
+
+      let cartItemContent = document.createElement('div');
+      cartItemContent.className = 'cart__item__content';
+      //console.log(cartItemContent);
+
+      cartItemContent.appendChild(cartItemContentDescription);
+      cartItemContent.appendChild(cartItemContentSettings);
+
+      let cartItem = document.createElement('article');
+      cartItem.className = 'cart__item';
+      cartItem.dataset.id = itemId;
+      cartItem.dataset.color = itemColor;
+      //console.log(cartItem);
+
+      cartItem.appendChild(cartItemImg);
+      cartItem.appendChild(cartItemContent);
+
+      sectionCart.appendChild(cartItem);
+    })
+    .catch((err) => {
+      // Do something for an error here
+      console.log(err);
+    });
 }
